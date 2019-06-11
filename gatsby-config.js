526238bfcdfafa63b1703e841378744fb3f4992b
valtermartins1301 <valtermartins.1301@gmail.com>
2019-06-11 16:56:10 -0300

    add offline suport

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Creditas`,
-    description: `Empréstimo com veículo em garantia | Creditas.`,
+    title: `Gatsby`,
+    description: `Gatsby and Netlify`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -28,8 +28,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,26 +9,26 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Step selected="1"/>
-    <form name="contact" netlify>
-      <h1>Nos conte sobre você!</h1>
-      <p>
-        Queremos te conhecer para podermos te atender melhor! E não se preocupe, todas as suas informações estão seguras ;)
-      </p>
 
+    <form name="contact" method="POST" data-netlify="true">
       <p>
-        <label> Nome:
-              <input type="text" name="name" placeholder="Insira o nome aqui" />
-        </label>
+        <label>Your Name: <input type="text" name="name" /></label>   
       </p>
       <p>
-        <label> Email:
-              <input type="email" name="email" placeholder="Insira seu email aqui" />
-        </label>
+        <label>Your Email: <input type="email" name="email" /></label>
       </p>
-
-      <div>
-        <button type="submit">Enviar</button>
-      </div>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
 
     <Link to="/page-2/">Go to page 2</Link>
