5bd4008e04e29c1827f80bedba77bb8acac2ddbe
valtermartins1301 <valtermartins.1301@gmail.com>
2019-06-12 11:31:06 -0300

    Fix build

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Step from "../components/step"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import Step from "../../components/step"
 
 const IndexPage = () => (
   <Layout>
