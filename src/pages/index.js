eba16f85542e270c86c7bef8b3bda7e31ec87ce9
valtermartins1301 <valtermartins.1301@gmail.com>
2019-06-14 16:17:28 -0300

    update form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Step from "../components/step"
-
-const ContactSection = ({classNames}) => {
-  return (
-    <section>
-      <h2>Contact information</h2>
-      <p>
-        <label for="name">
-          <span>Name: </span>
-        </label>
-        <input type="text" id="name" name="username"/>
-      </p>
-      <p>
-        <label for="mail">
-          <span>E-mail: </span>
-        </label>
-        <input type="email" id="mail" name="usermail"/>
-      </p>
-    </section>
-  )
-}
+import { LeadForm } from "lead-form"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Step selected="1"/>
 
-    <form name="contact" method="POST" data-netlify="true">
-      <h1>Simulation Form</h1>
-
-      <ContactSection/>
-    </form>
+    <LeadForm/>
 
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
